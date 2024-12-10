e4e00ee8905f3bb77ee6a468502b408af1f48d06
Muhammad Ayaz <69014774+webindustry059@users.noreply.github.com>
2024-12-10 22:37:00 +0500

    added esc_html in footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer class="d-flex text-center justify-content-center bg-dark mt-2 p-2 text-white">
- Designed for Task 1
+ <?php echo esc_html__('Designed for Task 1','task_1_td'); ?>
 </footer>
 <?php wp_footer(); ?>
